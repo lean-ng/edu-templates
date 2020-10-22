5bf17db05a7ed8ec5fd62f7c4b42ffffb4bed1d5
Micha Alt <ng@lean-stack.de>
2020-10-22 17:07:55 +0200

    Start off with first sample: property bindings.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PropertyBindingComponent } from './edu/property-binding/property-binding.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'property-binding', component: PropertyBindingComponent },
 ];
 
 @NgModule({
